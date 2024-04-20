0465041f319946103d89ac03270f7bfc8117c0f6
Simran <simrandsota56@gmail.com>
2024-04-20 20:38:29 +0530

    offcanvas added

--- src/pages/DashBoard/Questions.js
+++ src/pages/DashBoard/Questions.js
@@ -1,55 +1,61 @@
 const questions = [
   {
+    type: 'mcq',
     question: 'What is 2 + 2?',
     options: ['3', '4', '5', '6'],
     correctAnswer: '4'
   },
   {
+    type: 'mcq',
     question: 'Which planet is known as the Red Planet?',
     options: ['Earth', 'Mars', 'Venus', 'Jupiter'],
     correctAnswer: 'Mars'
   },
   {
+    type: 'mcq',
     question: 'What is the capital of France?',
     options: ['Madrid', 'Rome', 'Paris', 'Berlin'],
     correctAnswer: 'Paris'
   },
   {
+    type: 'short_answer',
+    question: 'Who painted the Mona Lisa?',
+    correctAnswer: 'Leonardo da Vinci'
+  },
+  {
+    type: 'short_answer',
+    question: 'Which element has the chemical symbol "Fe"?',
+    correctAnswer: 'Iron'
+  },
+  {
+    type: 'long_answer',
+    question: 'What is the tallest mountain in the world?',
+    correctAnswer: 'Mount Everest'
+  },
+  {
+    type: 'long_answer',
+    question: 'Who wrote "To Kill a Mockingbird"?',
+    correctAnswer: 'Harper Lee'
+  },
+  // Three new questions
+  {
+    type: 'mcq',
     question: 'What is the chemical symbol for water?',
     options: ['H2O', 'CO2', 'NaCl', 'O2'],
     correctAnswer: 'H2O'
   },
   {
-    question: 'Who wrote "To Kill a Mockingbird"?',
-    options: ['Harper Lee', 'J.K. Rowling', 'Ernest Hemingway', 'F. Scott Fitzgerald'],
-    correctAnswer: 'Harper Lee'
-  },
-  {
+    type: 'mcq',
     question: 'Which year did the Titanic sink?',
     options: ['1912', '1915', '1908', '1920'],
     correctAnswer: '1912'
   },
   {
+    type: 'mcq',
     question: 'What is the tallest mountain in the world?',
     options: ['K2', 'Mount Everest', 'Kangchenjunga', 'Lhotse'],
     correctAnswer: 'Mount Everest'
-  },
-  {
-    question: 'Who painted the Mona Lisa?',
-    options: ['Leonardo da Vinci', 'Pablo Picasso', 'Vincent van Gogh', 'Michelangelo'],
-    correctAnswer: 'Leonardo da Vinci'
-  },
-  {
-    question: 'Which element has the chemical symbol "Fe"?',
-    options: ['Iron', 'Gold', 'Silver', 'Copper'],
-    correctAnswer: 'Iron'
-  },
-  {
-    question: 'What is the capital of Japan?',
-    options: ['Tokyo', 'Beijing', 'Seoul', 'Bangkok'],
-    correctAnswer: 'Tokyo'
-  },
-  // Add more questions here...
+  }
 ];
 
 export default questions;
--- src/pages/DashBoard/Quiz.jsx
+++ src/pages/DashBoard/Quiz.jsx
@@ -1,26 +1,46 @@
 import React, { useState } from 'react';
-import "../../components/Sparkles.css";
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import heart from '../../assets/heart.png';
 import questions from '../DashBoard/Questions.js';
-// import questions from '../../components/Questions.js';
 import DialogBox from '../../components/DialogBox.jsx';
-// import DragAndDropExercise from '../../components/DragAndDropExercise.jsx'; // Import DragAndDropExercise component
+import "../../components/Sparkles.css";
+import "./Quiz.css";
 
 function Quiz() {
     const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
     const [answers, setAnswers] = useState([]);
     const [quizFinished, setQuizFinished] = useState(false);
+    const [isAnswerCorrect, setIsAnswerCorrect] = useState(false);
+    const [offCanvasOpen, setOffCanvasOpen] = useState(false);
+    const [checkAnswerVisible, setCheckAnswerVisible] = useState(true);
 
     const handleNextQuestion = () => {
         setCurrentQuestionIndex(currentQuestionIndex + 1);
+        setOffCanvasOpen(false);
+        setIsAnswerCorrect(false);
+        setCheckAnswerVisible(true);
     };
 
     const handleOptionClick = (option) => {
-        // Handle the logic when an option is clicked
         const updatedAnswers = [...answers];
         updatedAnswers[currentQuestionIndex] = option;
         setAnswers(updatedAnswers);
+    };
+
+    const handleAnswerChange = (event) => {
+        const updatedAnswers = [...answers];
+        updatedAnswers[currentQuestionIndex] = event.target.value;
+        setAnswers(updatedAnswers);
+    };
+
+    const checkAnswer = () => {
+        if (answers[currentQuestionIndex] === questions[currentQuestionIndex].correctAnswer) {
+            setIsAnswerCorrect(true);
+        } else {
+            setIsAnswerCorrect(false);
+        }
+        setOffCanvasOpen(true);
+        setCheckAnswerVisible(false);
     };
 
     const handleFinishQuiz = () => {
@@ -54,21 +74,44 @@ function Quiz() {
                 ) : (
                     <>
                         <h3 className='vt323-regular'>{currentQuestion.question}</h3>
-                        <div className="grid grid-cols-2 gap-4">
-                            {currentQuestion.options.map((option, index) => (
-                                <button key={index} className="option w-20 h-20 border-solid border-2 rounded-md border-blue-100" onClick={() => handleOptionClick(option)}>
-                                    {option}
-                                </button>
-                            ))}
-                        </div>
-                        {/* <DragAndDropExercise questions={questions} /> Add DragAndDropExercise component here */}
-                        {currentQuestionIndex < questions.length - 1 ? (
-                            <div className="button">
-                                <a href="#" className="vt323-regular" onClick={handleNextQuestion}><p>Next</p></a>
+                        {currentQuestion.type === 'mcq' ? (
+                            <div className="grid grid-cols-2 gap-4">
+                                {currentQuestion.options.map((option, index) => (
+                                    <button key={index} className="option w-20 h-20 border-solid border-2 rounded-md border-blue-100" onClick={() => handleOptionClick(option)}>
+                                        {option}
+                                    </button>
+                                ))}
                             </div>
                         ) : (
+                            <textarea 
+                                className="w-64 h-32 border-solid border-2 rounded-md border-blue-100 p-2"
+                                value={answers[currentQuestionIndex] || ''}
+                                onChange={handleAnswerChange}
+                            />
+                        )}
+                        {currentQuestionIndex < questions.length - 1 ? (
+                            <>
+                                {checkAnswerVisible && (
+                                    <div className="button">
+                                        <button className="btn btn-primary vt323-regular" onClick={checkAnswer} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasBottom" aria-controls="offcanvasBottom">Check Answer</button>
+                                    </div>
+                                )}
+                                <div className={`offcanvas offcanvas-bottom ${offCanvasOpen ? 'show' : ''}`} tabindex="-1" id="offcanvasBottom" aria-labelledby="offcanvasBottomLabel">
+                                    <div className="offcanvas-header">
+                                        <h5 className="offcanvas-title" id="offcanvasBottomLabel">Answer</h5>
+                                        <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                                    </div>
+                                    <div className="offcanvas-body small">
+                                        <p className={`vt323-regular ${isAnswerCorrect ? 'correct' : 'incorrect'}`}>
+                                            {isAnswerCorrect ? 'Correct!' : 'Incorrect!'}
+                                        </p>
+                                        <button className='btn btn-primary vt323-regular' onClick={handleNextQuestion}>Next</button>
+                                    </div>
+                                </div>
+                            </>
+                        ) : (
                             <div className="button">
-                                <a href="#" className="vt323-regular" onClick={handleFinishQuiz}><p>Finish</p></a>
+                                 <button className="btn btn-primary vt323-regular" onClick={handleFinishQuiz}>Finish</button>
                             </div>
                         )}
                     </>
